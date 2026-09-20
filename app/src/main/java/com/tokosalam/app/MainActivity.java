c2e92bbdf39d058426811467606d16b179659b03
asesorispriawanita-pixel <asesorispriawanita@gmail.com>
2026-09-20 12:21:56 +0700

    New MainActivity.java

--- app/src/main/java/com/tokosalam/app/MainActivity.java
+++ app/src/main/java/com/tokosalam/app/MainActivity.java
@@ -2,35 +2,105 @@ package com.tokosalam.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
-    private WebView webView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+private WebView webView;
+private ValueCallback<Uri[]> filePathCallback;
 
-        webView = new WebView(this);
-        setContentView(webView);
+private static final int FILE_CHOOSER_REQUEST = 1001;
 
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.loadUrl("file:///android_asset/index.html");
+    webView = new WebView(this);
+    setContentView(webView);
+
+    WebSettings settings = webView.getSettings();
+
+    settings.setJavaScriptEnabled(true);
+    settings.setDomStorageEnabled(true);
+    settings.setAllowFileAccess(true);
+    settings.setAllowContentAccess(true);
+
+    webView.setWebViewClient(new WebViewClient());
+
+    webView.setWebChromeClient(new WebChromeClient() {
+
+        @Override
+        public boolean onShowFileChooser(
+                WebView webView,
+                ValueCallback<Uri[]> filePathCallback,
+                FileChooserParams fileChooserParams) {
+
+            if (MainActivity.this.filePathCallback != null) {
+                MainActivity.this.filePathCallback.onReceiveValue(null);
+            }
+
+            MainActivity.this.filePathCallback = filePathCallback;
+
+            Intent intent = fileChooserParams.createIntent();
+
+            try {
+                startActivityForResult(intent, FILE_CHOOSER_REQUEST);
+            } catch (Exception e) {
+                MainActivity.this.filePathCallback = null;
+                return false;
+            }
+
+            return true;
+        }
+    });
+
+    webView.loadUrl("file:///android_asset/index.html");
+}
+
+@Override
+protected void onActivityResult(
+        int requestCode,
+        int resultCode,
+        Intent data) {
+
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode == FILE_CHOOSER_REQUEST) {
+
+        if (filePathCallback == null) {
+            return;
+        }
+
+        Uri[] results = null;
+
+        if (resultCode == RESULT_OK && data != null) {
+
+            Uri uri = data.getData();
+
+            if (uri != null) {
+                results = new Uri[]{uri};
+            }
+        }
+
+        filePathCallback.onReceiveValue(results);
+        filePathCallback = null;
     }
+}
 
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) webView.goBack();
-        else super.onBackPressed();
+@Override
+public void onBackPressed() {
+
+    if (webView.canGoBack()) {
+        webView.goBack();
+    } else {
+        super.onBackPressed();
     }
+}
+
 }
